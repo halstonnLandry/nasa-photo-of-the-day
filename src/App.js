273197dac627af9289f7658e1c6664dbffb2d6dc
Halstonn Landry <hallrstein@gmail.com>
2020-03-19 21:34:08 -0500

    nasa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+import styled from 'styled-components';
+
 
 function App() {
+  const [data, setData]= useState ([]);
+ useEffect(()=>{
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+  .then(response=>{
+    setData(response.data);
+    console.log(response.data);
+  })
+  .catch(error=>{
+    console.log(error);
+  });
+ },[]) 
+const BodyDiv= styled.div`
+ width:80%
+ margin:auto;
+ background-color:#000000;
+ color:#00ffff;
+ text-shadow: 0px 0px 15px #00ffff;
+`;
+const LimitImg= styled.img`
+width:80%;
+height:auto;
+`;
+const ShadowBox=styled.div`
+box-shadow: 0 0 10px #ff00ff;
+`
+
   return (
-    <div className="App">
+    <BodyDiv className="App">
+
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        {data.date}
       </p>
-    </div>
+      <p>
+        {data.explanation}
+      </p>
+      <ShadowBox>
+        <LimitImg src={data.hdurl}/>
+      </ShadowBox>
+      <p>
+        {`copyright: ${data.copyright}`}
+      </p>
+    </BodyDiv>
   );
 }
 
